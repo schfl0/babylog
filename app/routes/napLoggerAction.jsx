37807e25bbc4fbce2f86e05b9056dcc96ff3f2af
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-09 13:14:02 +0200

    Update napLoggerAction.jsx

--- app/routes/napLoggerAction.jsx
+++ app/routes/napLoggerAction.jsx
@@ -2,13 +2,19 @@ import { redirect } from "react-router";
 import { logNap } from "../../actions.server.js";
 
 export async function action({ context, request }) {
-  if (!context?.session?.user) throw redirect("/");
-
+  // if (!context?.session?.user) throw redirect("/");
+const resSession = await fetch("https://babylog.fl0dev.net/api/auth/session", {
+    headers: {
+      Cookie: request.headers.get("Cookie") ?? "",
+    },
+    credentials: "include",
+  });
+  const session = await resSession.json();
   const formData = await request.formData();
   const triggerNap = formData.get("triggerNap");
   console.log("Nap:", triggerNap);
 
-  const res = await logNap(context.session.user.email, triggerNap);
+  const res = await logNap(session?.user.email, triggerNap);
   console.log(res);
 }
 
