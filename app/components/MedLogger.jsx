3607a8de96713664026d2e4325ea1afd4819b9a3
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-09 15:27:07 +0200

    Update MedLogger.jsx

--- app/components/MedLogger.jsx
+++ app/components/MedLogger.jsx
@@ -1,1 +1,63 @@
+import { useFetcher } from "react-router";
+import { useState, useEffect } from "react";
 
+export default function FoodLogger({ session, logger }) {
+  const fetcher = useFetcher();
+  const [inputMed, setInputMed] = useState("");
+  const [inputG, setInputG] = useState("");
+
+  useEffect(() => {
+    if (fetcher.state === "idle") {
+      setInputMed("");
+      setInputG("");
+    }
+  }, [fetcher.state]);
+
+  return (
+    <div className="px-2 py-4 shadow-md">
+      <fetcher.Form
+        method="delete"
+        action="/delete-logger"
+        className="flex justify-end"
+      >
+        <input type="hidden" name="deleteLogger" value={logger} />
+        <button
+          type="submit"
+          className="cursor-pointer transition-all hover:opacity-50"
+        >
+          ❌
+        </button>
+      </fetcher.Form>
+
+      <fetcher.Form method="post" action="/med-logger">
+        <div className="mt-2 mb-2 flex items-center justify-center gap-2">
+          <label htmlFor="med">Medication:</label>
+          <input
+            className="w- border border-gray-400 px-1 py-0.5"
+            type="text"
+            name="med"
+            id="med"
+            value={inputMed}
+            onChange={(e) => setInputMed(e.target.value)}
+          />
+
+          <label htmlFor="g">g:</label>
+          <input
+            className="w-14 border border-gray-400 px-1 py-0.5"
+            type="number"
+            name="g"
+            id="g"
+            value={inputG}
+            onChange={(e) => setInputG(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-2 w-full cursor-pointer bg-neutral-200 p-2 transition-all hover:opacity-70"
+        >
+          💊 Medication
+        </button>
+      </fetcher.Form>
+    </div>
+  );
+}
