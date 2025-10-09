7169fe98826d3ec3d39a0d35d54301368dd82404
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-09 13:11:44 +0200

    Update bottleLoggerAction.jsx

--- app/routes/bottleLoggerAction.jsx
+++ app/routes/bottleLoggerAction.jsx
@@ -1,13 +1,20 @@
 import { redirect } from "react-router";
 import { logBottle } from "../../actions.server.js";
 
-export async function action({ context, request }) {
+export async function action({ request }) {
   if (!context?.session?.user) throw redirect("/");
+  const resSession = await fetch("https://babylog.fl0dev.net/api/auth/session", {
+    headers: {
+      Cookie: request.headers.get("Cookie") ?? "",
+    },
+    credentials: "include",
+  });
+  const session = await resSession.json();
 
   const formData = await request.formData();
   const ml = formData.get("ml");
   const date = new Date();
-  await logBottle(context.session.user.email, ml, date);
+  await logBottle(session?.user.email, ml, date);
 }
 
 export async function loader() {
