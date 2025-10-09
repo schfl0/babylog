0270b99ca6bca709bedcaac3855a6e16de8c47f1
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-09 11:45:56 +0200

    Update Nav.jsx

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,8 +1,17 @@
 import { NavLink } from "react-router";
-import { useSession } from "../sessionContext";
+import { useEffect, useState } from "react";
 
 export default function Nav() {
-  const { session, csrfToken } = useSession();
+
+  const [csrfToken, setCsrfToken] = useState("")
+
+  useEffect(()=> {
+    (async()=>{
+      const res = await fetch("/api/auth/csrf", {credentials: "include"});
+      const data = await res.json();
+      setCsrfToken(data.csrfToken);
+    })()
+  }, [])
   return (
     <nav className="flex items-center gap-6 p-4 text-sm">
       <NavLink
@@ -21,7 +30,7 @@ export default function Nav() {
       >
         📝 Logs
       </NavLink>
-      {session && (
+      {session && csrfToken (
         <form
           action="https://babylog.fl0dev.net/api/auth/signout"
           method="POST"
@@ -30,7 +39,7 @@ export default function Nav() {
           <input
             type="hidden"
             name="csrfToken"
-            value={csrfToken?.csrfToken || ""}
+            value={csrfToken}
           />
           <button
             type="submit"
