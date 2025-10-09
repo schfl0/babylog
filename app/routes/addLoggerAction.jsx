7bbf11442b7c0742de9b7b33d6ed971fa300103d
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-09 13:05:46 +0200

    Update addLoggerAction.jsx

--- app/routes/addLoggerAction.jsx
+++ app/routes/addLoggerAction.jsx
@@ -1,13 +1,13 @@
 import { redirect } from "react-router";
 import { addLogger } from "../../actions.server.js";
 export async function action({ request }) {
-   const res = await fetch("https://babylog.fl0dev.net/api/auth/session", {
+   const resSession = await fetch("https://babylog.fl0dev.net/api/auth/session", {
     headers: {
       Cookie: request.headers.get("Cookie") ?? "",
     },
     credentials: "include",
   });
-  const session = await res.json();
+  const session = await resSession.json();
   const formData = await request.formData();
   const logger = formData.get("addLogger");
   const res = await addLogger(
