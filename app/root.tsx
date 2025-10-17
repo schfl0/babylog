9e13a22c3bf5e5b50884b81914bbcd13a692e553
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-17 11:45:50 +0200

    Update root

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import { SessionProvider } from "./sessionContext";
 import Nav from "../app/components/Nav.jsx";
 
 import { APP_ORIGIN} from "../config";
+console.log("APP ORIGIN:", APP_ORIGIN)
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
