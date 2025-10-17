757e8cdf396e65efa394acac48990b218ec5b920
fl0 <77931563+schfl0@users.noreply.github.com>
2025-10-17 12:23:42 +0200

    Another update

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,9 +5,7 @@ import { useSession } from "../sessionContext";
 import {APP_ORIGIN} from "../../config";
 console.log("WINDOW:", typeof window);
 console.log("NAV APP ORIGIN:", APP_ORIGIN);
-console.log("WINDOW LOCATION:", window?.location);
-
-
+if (window !== "undefined") console.log("WINDOW LOCATION:", window.location);
 
 export default function Nav() {
 const {session} = useSession();
